1f2233f327d96a885efc5820ca97a2b13d6e6da1
Daniel Sprohar <daniel.sprohar@gmail.com>
2024-09-08 23:55:11 -0500

    refactor: Update Section model with position field

--- prisma/migrations/20240909045451_init/migration.sql
+++ prisma/migrations/20240909045451_init/migration.sql
@@ -61,10 +61,12 @@ CREATE TABLE `Project` (
 CREATE TABLE `Section` (
     `id` BINARY(16) NOT NULL,
     `name` VARCHAR(32) NOT NULL,
+    `position` INTEGER NOT NULL DEFAULT 0,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
     `project_id` BINARY(16) NOT NULL,
 
+    UNIQUE INDEX `Section_project_id_position_key`(`project_id`, `position`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- src/features/sections/dto/create-section.dto.ts
+++ src/features/sections/dto/create-section.dto.ts
@@ -1,9 +1,12 @@
-import { IsString, IsUUID, MaxLength } from "class-validator";
+import { IsNumberString, IsString, IsUUID, MaxLength } from "class-validator";
 
 export class CreateSectionDto {
   @IsString()
   @MaxLength(32)
   name: string;
+
+  @IsNumberString()
+  position: number;
 
   @IsUUID()
   projectId: string;
